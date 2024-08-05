7ce2a0f6752a853c62920f90d85f02bb9ba378c9
asalam3312 <sala981013@gmail.com>
2024-08-05 12:33:25 -0500

    first changes

---
+++ my-proyect/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <div className="navbar-brand"><Link to="/">Cool Media</Link></div>
+    
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <form className="d-flex">
+        <button className="btn btn-outline-success" type="submit"><Link to="/SignIn">Sign In</Link></button>
+        <button className="btn btn-outline-success" type="submit"><Link to="/LogIn">Log in</Link></button>
+      </form>
+    </div>
+  </div>
+</nav>
+  )
+}
+
+export default Navbar
