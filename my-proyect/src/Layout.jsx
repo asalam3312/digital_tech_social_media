53d224a83e6e8f4aa84284bb7a501f4b49a64815
asalam3312 <sala981013@gmail.com>
2024-08-05 13:14:07 -0500

    routes and global state working

--- my-proyect/src/Layout.jsx
+++ my-proyect/src/Layout.jsx
@@ -3,20 +3,24 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './views/Home.jsx'
 import Contact from './views/Contact.jsx'
 import NotFound from './views/NotFound.jsx'
-import LogIn from './views/LogIn.jsx';
+import Navbar from './components/Navbar.jsx'
+import injectContext from "./js/store/appContext";
 import SignIn from './views/SignIn.jsx';
-import Navbar from './components/Navbar.jsx';
+import LogIn from './views/LogIn.jsx';
 
 const Layout = () => {
+
+  const basename = process.env.BASENAME || "";
+
   return (
     <div>
-        <BrowserRouter>
-        <Navbar />
+        <BrowserRouter basename={basename}>
+            <Navbar/>
             <Routes>
                 <Route exact path="/" element={<Home/>}/>
                 <Route exact path="/contact" element={<Contact/>}/>
-                <Route exact path="/LogIn" element={<LogIn/>}/>
                 <Route exact path="/SignIn" element={<SignIn/>}/>
+                <Route exact path="/LogIn" element={<LogIn/>}/>
                 <Route exact path="/*" element={<NotFound/>}/>
             </Routes>
         </BrowserRouter>
@@ -24,5 +28,5 @@ const Layout = () => {
   )
 }
 
-export default Layout
+export default injectContext(Layout)
 
--- my-proyect/src/views/Home.jsx
+++ my-proyect/src/views/Home.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Context } from '../js/store/appContext'
 
 const Home = () => {
+    const { store, actions } = useContext(Context)
   return (
-    <div>Home</div>
+    <div>Home
+        <h5>Personas en global:{store.personas}</h5>
+    </div>
   )
 }
 
